249bd421473139a7fcb2ef6856f0da3cf9c3df44
erival.ahmad <erival.ahmad@bull.co.id>
2025-04-09 11:34:13 +0700

    update section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 //builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
@@ -27,6 +28,7 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 */
+app.UseCors(x => x.AllowAnyHeader().AllowAnyOrigin().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
